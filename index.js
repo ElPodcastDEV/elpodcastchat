24c083492556f7915b9d722e5a5ad81ea9fd6885
Carlos Flores <carlos.flores@resuelve.mx>
2019-12-03 17:52:12 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { resolve } = require('path')
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+http.listen(3200, function () {
+  console.log('listening on *:3200')
+})
+
+io.on('connection', socket => {
+  socket.on('disconnect', () => {})
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+  })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, './index.html'))
+})
