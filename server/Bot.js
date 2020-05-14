5f3d6b624aadfc077333ec1fc4be89089cff8a96
Zero Dragon <carlos@floresbenavides.com>
2020-05-14 17:39:41 -0500

    Move commands to responses file

---
+++ server/Bot.js
@@ -0,0 +1,84 @@
+const Auth = require('./Auth')
+
+class Bot {
+  constructor (inputBrain, io) {
+    this.brain = inputBrain
+    this.io = io
+    this.auth = new Auth({ secret: process.env.SECRET })
+    this.commands = {}
+  }
+
+  async reportConfig (socket) {
+    const current = await this.brain.hgetall('system-config')
+    this.sendSystem(socket, JSON.stringify(current))
+  }
+
+  async getConfig () {
+    const conf = this.brain.hgetall('system-config')
+    if (conf === null) return conf
+    return {
+      episode: 1,
+      hosts: []
+    }
+  }
+
+  when (command, callback) {
+    this.commands[command] = callback
+  }
+
+  on (command, socket, params, user) {
+    if (!this.commands[command]) {
+      return this.sendSystem(socket, `command ${command} no existe`)
+    }
+    return this.commands[command](socket, params, user)
+  }
+
+  sendSystem (socket, message) {
+    socket.emit('chat message', JSON.stringify({
+      username: 'SYSTEM',
+      uid: this.uuid(),
+      message
+    }))
+  }
+
+  broadcastSystem (message) {
+    this.io.emit('chat message', JSON.stringify({
+      username: 'SYSTEM',
+      uid: this.uuid(),
+      message
+    }))
+  }
+
+  sendSystemData (socket, action, value, key = 'chat-action') {
+    socket.emit('system message', JSON.stringify({
+      key,
+      action,
+      value
+    }))
+  }
+
+  broadcastSystemData (key, action, value) {
+    this.io.emit('system message', JSON.stringify({
+      key, action, value
+    }))
+  }
+
+  uuid () {
+    const uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      var r = Math.random() * 16 | 0; var v = c === 'x' ? r : (r & 0x3 | 0x8)
+      return v.toString(16)
+    })
+    return `${new Date().getTime()}-${uid}`
+  }
+
+  async fnIsAdmin (userName) {
+    if (!userName) return false
+    const permissions = await this.brain.hget(
+      userName, 'permissions'
+    ) || JSON.stringify({})
+    const { isAdmin } = JSON.parse(permissions)
+    return isAdmin
+  }
+}
+
+module.exports = Bot
--- server/responses.js
+++ server/responses.js
@@ -1,7 +1,116 @@
-const Bot = require('./bot')
+const Bot = require('./Bot')
 
 let brain = null
 let bot = null
+
+const setupResponses = () => {
+  bot.when('/register', async (socket, [password, email]) => {
+    if (!password || !email) {
+      bot.sendSystem(socket, 'debes especificar un password y email')
+      bot.sendSystem(socket, '/register tupassword tuemail')
+      bot.sendSystem(socket, '(sin espacios el password o email)')
+      return
+    }
+    const data = await bot.brain.hget(socket.userName, 'email')
+    if (data) return bot.sendSystem(socket, 'Este username ya está registrado')
+    const token = bot.auth.generateToken({ userName: socket.userName })
+    await bot.brain.hset(socket.userName, 'password', password)
+    await bot.brain.hset(socket.userName, 'email', email)
+    bot.sendSystemData(socket, 'token', token, 'token')
+    bot.sendSystem(socket, 'Listo')
+    bot.sendSystem(socket, 'Ahora reclama tu username mandando el comando')
+    bot.sendSystem(socket, '/password <password>')
+    bot.sendSystem(socket, 'y así nadie más lo podrá usar hasta que te vayas')
+  })
+  bot.when('/password', async (socket, [password]) => {
+    const data = await bot.brain.hget(socket.userName, 'password')
+    if (data === password) {
+      socket.reclaiming = false
+      const sockets = bot.io.sockets.clients()
+      Object.keys(sockets.connected).forEach(id => {
+        const connectedSocket = sockets.connected[id]
+        const [connId, connUser] = [connectedSocket.id, connectedSocket.userName]
+        const [soId, soUser] = [socket.id, socket.userName]
+        if (connId !== soId && connUser === soUser) {
+          connectedSocket.reclaiming = true
+        }
+      })
+      const token = bot.auth.generateToken({ userName: socket.userName })
+      bot.sendSystem(socket, 'Listo ya puedes escribir')
+      bot.sendSystemData(socket, 'token', token, 'token')
+      return
+    }
+    bot.sendSystem(socket, 'Password incorrecto')
+  })
+  bot.when('/changePassword', async (socket, [password, newPass]) => {
+    const data = await bot.brain.hget(socket.userName, 'password')
+    if (data === password) {
+      const token = bot.auth.generateToken({ userName: socket.userName })
+      bot.sendSystem(socket, 'Contraseña actualizada')
+      await bot.brain.hset(socket.userName, 'password', newPass)
+      bot.sendSystemData(socket, 'token', token, 'token')
+      return
+    }
+    bot.sendSystem(socket, 'Password incorrecto')
+  })
+  bot.when('/validateToken', async (socket, token) => {
+    const isVerified = bot.auth.verifyToken(token)
+    if (isVerified) {
+      const userName = bot.auth.parseToken(token).meta.userName
+      socket.userName = userName
+      const password = await bot.brain.hget(socket.userName, 'password')
+      bot.on('/password', socket, [password])
+    }
+  })
+  bot.when('/getTokenData', (_socket, token) => {
+    if (!token) return false
+    const isVerified = bot.auth.verifyToken(token)
+    if (isVerified) {
+      return bot.auth.parseToken(token).meta
+    }
+    return false
+  })
+  bot.when('/clearShowcase', async (_socket, _params, user) => {
+    const isAdmin = await bot.fnIsAdmin(user)
+    if (!isAdmin) return
+    bot.broadcastSystemData('chat-action', 'removeShowcase')
+  })
+  bot.when('/setupChat', async (socket, [key, value], user) => {
+    const isAdmin = await bot.fnIsAdmin(user)
+    if (!isAdmin) return
+    if (!key || !value) {
+      return bot.reportConfig(socket)
+    }
+    await bot.brain.hset('system-config', key, value)
+    bot.reportConfig(socket)
+  })
+  bot.when('/clearSetupKey', async (socket, [key], user) => {
+    const isAdmin = await bot.fnIsAdmin(user)
+    if (!isAdmin || !key) return
+    await bot.brain.hdel('system-config', key)
+    bot.reportConfig(socket)
+  })
+  bot.when('/help', async (socket, params, user) => {
+    bot.sendSystem(socket, '##########################################')
+    bot.sendSystem(socket, 'COMANDOS DISPONIBLES:')
+    bot.sendSystem(socket, '/register <tupassword> <tuemail>')
+    bot.sendSystem(socket, '/password <password>')
+    bot.sendSystem(socket, '/changePassword <password> <nuevopassword>')
+    bot.sendSystem(socket, '/clear')
+    bot.sendSystem(socket, '/logout')
+    bot.sendSystem(socket, '/img')
+    bot.sendSystem(socket, '/gif <url/a/la/imagen.gif>')
+    bot.sendSystem(socket, '##########################################')
+    if (!await bot.fnIsAdmin(user)) return
+    bot.sendSystem(socket, 'COMANDOS PARA ADMIN')
+    bot.sendSystem(socket, '/sudo')
+    bot.sendSystem(socket, '/exit')
+    bot.sendSystem(socket, '/pip')
+    bot.sendSystem(socket, '/clearShowcase')
+    bot.sendSystem(socket, '/setupChat')
+    bot.sendSystem(socket, '##########################################')
+  })
+}
 
 const sendSystem = (socket, message) => {
   bot.sendSystem(socket, message)
@@ -72,5 +181,6 @@ module.exports = {
   initializeBrain: (inputBrain, io) => {
     brain = inputBrain
     bot = new Bot(brain, io)
+    setupResponses()
   }
 }
