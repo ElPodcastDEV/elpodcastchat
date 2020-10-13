7baf3ca4d8bd0f03e0f8485ffe1dbc11cbd42ff6
Zero Dragon <carlos@floresbenavides.com>
2020-10-13 13:40:30 -0500

    Update swanros photo

--- front/components/interests.vue
+++ front/components/interests.vue
@@ -42,7 +42,7 @@
 .interest
   .hosts
     .host
-      .avatar: img(src="https://pbs.twimg.com/profile_images/1308054967471812612/GkcLbRkG_400x400.jpg")
+      .avatar: img(src="https://pbs.twimg.com/profile_images/1316014643387613187/Dfa7-G8T_400x400.jpg")
       .details
         a(href="https://twitter.com/swanros" target="_blank") Oscar Swanros
         a(href="https://twitter.com/swanros" target="_blank").link @Swanros
